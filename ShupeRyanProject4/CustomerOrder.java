226383c5ea895360bc50fc324a3276a17f3e6dc8
IamWaffle <ryanthecat17@gmail.com>
2017-11-16 22:50:49 -0500

    Added Getters and setters

--- ShupeRyanProject4/CustomerOrder.java
+++ ShupeRyanProject4/CustomerOrder.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class CustomerOrder{
 	
 	final private double COST_GAGET = 5.00;          //Cost of a gadget CONSTANT
@@ -22,16 +24,132 @@ public class CustomerOrder{
 	
 	/****CONSTRUCTORS****/
 	
+	//no arg Constructor
 	public CustomerOrder() {
 		
 	}
 	
-	/****GETTERS****/
+	//arg Constructor
+	public CustomerOrder(String cName, String cNum, String pNum, int numG, int numW, char tCode, char sCode, int dCode) {
+		customerNumber = cNum;
+		customerName = cName;
+		phoneNum = pNum;
+		numGadgets = numG;
+		numWidgets = numW;
+		taxCode = tCode;
+		shippingCode = sCode;
+		discountCode = dCode;
+	}
 	
+	//copy Constructor
+	public CustomerOrder(CustomerOrder original) {
+		customerNumber = original.customerNumber;
+		customerName = original.customerName;
+		phoneNum = original.phoneNum;
+		numGadgets = original.numGadgets;
+		numWidgets = original.numWidgets;
+		taxCode = original.taxCode;
+		shippingCode = original.shippingCode;
+		discountCode = original.discountCode;
+	}
 	
 	/****SETTERS****/
 	
+	public void setCustomerNumber(String num) {
+		customerNumber = num;
+	}
+	
+	public void setCustomerName(String num) {
+		customerName = num;
+	}
+	
+	public void setPhoneNum(String num) {
+		phoneNum = num;
+	}
+	
+	public void setNumGadgets(int num) {
+		numGadgets = num;
+	}
+	
+	public void setNumWidgets(int num) {
+		numWidgets = num;
+	}
+	
+	public void setTaxCode(char c) {
+		if (c == 'N') {
+			taxCode = c;
+		} 
+		else {
+			taxCode = 'T';
+		}
+	}
+	
+	public void setShippingCode(char c) {
+		if (c == 'O' || c == 'P') {
+			shippingCode = c;
+		} 
+		else {
+			shippingCode = 'S';
+		}
+	}
+	
+	public void setDiscountCode(int num) {
+		if (num == 1 || num == 2) {
+			discountCode = num;
+		} 
+		else {
+			discountCode = 0;
+		}
+	}
+	
+	/****GETTERS
+	 * @return ****/
+	
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	
+	public int getNumGadgets() {
+		return numGadgets;
+	}
+	
+	public int getNumWidgets() {
+		return numWidgets;
+	}
+	
+	public char getTaxCode() {
+		return taxCode;
+	}
+	
+	public char getShippingCode() {
+		return shippingCode;
+	}
+	
+	public int getDiscountCode() {
+		return discountCode;
+	}
+	
 	/****METHODS****/
+	
+	public String toString() {
+		String output = null;
+		
+		return output;
+	}
+	
+	public boolean equals() {
+		boolean tf = false;
+		return tf;
+		
+	}
 	
 	
 }
