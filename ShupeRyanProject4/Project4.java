3309d8786bfcee1876c0761e78fc01d3667e0629
IamWaffle <ryanthecat17@gmail.com>
2017-12-07 01:54:48 -0500

    DONE

--- ShupeRyanProject4/Project4.java
+++ ShupeRyanProject4/Project4.java
@@ -1,7 +1,5 @@
-package project3;
-
 import javax.swing.JOptionPane;
-import java.text.DecimalFormat;
+
 
 /*********************************************************************************** * 
 ------------------------------------------------------------------------------------ 
@@ -12,56 +10,144 @@ import java.text.DecimalFormat;
 *     Author E-mail: shuper@etsu.edu 
 *     Course-Section: CSCI-1250-004
 *     Creation Date: 11/20/17 
-*     Date of Last Modification: 11/29/17 
+*     Date of Last Modification: 12/06/17 
 * ------------------------------------------------------------------------------------ */
+
+/*********************************************************************************** 
+ * Class Name: Project4 <br> 
+ *  Class Purpose: Project 4 <br> 
+ *  
+ *   <hr> 
+ *   Date created: 12/07/17 <br> 
+ *   Date last modified: 12/07/17 
+ *   @author Ryan Shupe */
 
 public class Project4{
 	
+	/******************************************************************************** 
+	* Method Name: main <br> 
+	* Method Purpose:  Driver method for the entire program. Initialize variables, call methods    
+	*      <br> 
+	* 
+	* <hr> 
+	* Date created: 12/07/17 <br> 
+	* Date last modified: 12/07/17 <br> 
+	* 
+	* <hr> 
+	* 
+	*  
+	*  
+	*@param args  Command Line Arguments 
+	* <hr> 
+	*/
+	
 	public static void main(String[] args){
-		CustomerOrder order1 = null;
-		CustomerOrder order2 = null;
-		CustomerOrder order3 = null;
-		int mainChoice = 0;
-		int subChoice = 0;
-		int numOrders = 0;
-		final int MAX_ORDERS = 3;
-		String output = "";
+		CustomerOrder order1 = null;			//order 1 
+		CustomerOrder order2 = null;			//order 2
+		CustomerOrder order3 = null;			//order 3
+		
+		int mainChoice = 0;						//number chosen at the displayMenu()
+		int subChoice = 0;						//number chosen at the displaySubMenu()
+		int numOrders = 0;						//number of orders in the system
+		String output = "";						//string for output
 		
 		//Display the welcome message
 		JOptionPane.showMessageDialog(null,"Welcome to the Customer Order"
 			+ " System.  You may place up to 3 orders in our system.");
 		
-		
 		do{
-			//TO DO: call displayMenu to get the user's selection
-	
+		
 			mainChoice = displayMenu(numOrders);
 			
 			switch(mainChoice){
-				case 1: //TO DO: place a new order - you have three orders to work with.  Make sure
-						//that when inputting the order information you put it in the appropriate
-						//order object (order1, order2, or order 3)
-						
-						
+
+				case 1:
+					
+					if (numOrders == 0 ) {
+						order1 = inputOrder();
+						numOrders = 1;
+					}
+					else if(numOrders == 1 ) {
+						order2 = inputOrder();
+						numOrders = 2;
+					}
+					else {
+						order3 = inputOrder();
+						numOrders = 3;
+					}
+	
 						break;
 				
-				case 2: //TO DO: display all orders - if there are no orders, an appropriate message
-						//should be displayed.  Otherwise, you should display all orders that have
-						//actually been placed
-						
-						
+				case 2: 
+				
+					if(numOrders == 0 ) {
+						JOptionPane.showMessageDialog(null, "There are no orders in the system.");
+					}
+					else if (numOrders == 1){
+						output = order1.toString();
+						JOptionPane.showMessageDialog(null, output);
+					}
+					else if (numOrders == 2) {
+						output = order1.toString() + order2.toString();
+						JOptionPane.showMessageDialog(null, output);
+					}
+					else {
+						output = order1.toString() + order2.toString() + order3.toString();
+						JOptionPane.showMessageDialog(null, output);
+					}
 						break;
-				case 3: //TO DO: display a specific order - if there are no orders, an appropriate
-						//message should be displayed.  Otherwise, you should display a submenu
-						//of the orders that have been placed so the user can select which one
-						//to display.  It should then display the appropriate order.
 						
+				case 3: 
 						
+					if(numOrders == 0 ) {
+						JOptionPane.showMessageDialog(null, "There are no orders in the system.");
+					}
+					
+					else if(numOrders == 1) {
+						subChoice = displaySubMenu(order1, null, null, numOrders);
+					}
+					
+					else if(numOrders == 2) {
+						subChoice = displaySubMenu(order1, order2, null, numOrders);
+					}
+					else if(numOrders == 3) {
+						subChoice = displaySubMenu(order1, order2, order3, numOrders);
+					}
+					
+					
+					if(subChoice == 1) {
+						JOptionPane.showMessageDialog(null, order1);
+					}
+					else if (subChoice == 2) {
+						JOptionPane.showMessageDialog(null, order2);
+					}
+					else {
+						JOptionPane.showMessageDialog(null, order3);
+					}
+	
 						break;
 			}//end switch
 		} while (mainChoice != 4);
 		
 	}
+	
+	/******************************************************************************** 
+	* Method Name: displayMenu <br> 
+	* Method Purpose:  Display a menu with options to choose from. Has input validation where you can only choose a 1 2 3 or 4.   
+	*      <br> 
+	* 
+	* <hr> 
+	* Date created: 12/07/17 <br> 
+	* Date last modified: 12/07/17 <br> 
+	* 
+	* <hr> 
+	* 
+	*  
+	*  
+	*@param numOrders int
+	*@return selection
+	* <hr> 
+	*/
 	
 	public static int displayMenu(int numOrders)
 	{
@@ -77,43 +163,116 @@ public class Project4{
 					+ "\n\n You currently have " + numOrders + " orders in the system."
 					+ "\n\n Selection: ";
 		
-		while(selection > 4 && selection <=0) {
+		input = JOptionPane.showInputDialog(menu);
+		selection = Integer.parseInt(input);
+		
+		while(selection > 4 && selection < 0) {
 			input = JOptionPane.showInputDialog(menu);
 			selection = Integer.parseInt(input);
 		}
 		return selection;
 	}
 	
-	//TO DO: this method should display the orders available to view.  This would only be orders
-	//that have been placed.  For ex, if only one order has been placed, only order1 should show
-	//up in the submenu to be selected.  The customer name and the balance due should be shown 
-	//For ex. -> 1. John Smith  Balance Due: $123.45
-	//			 2. Jane Doe  Balance Due: $234.56
-	//			 Selection: 
-	//You should also do input validation to be sure that the user's selection is in fact a valid
-	//selection from the submenu. Balance Due should be shown with a currency formatting (dollar sign
-	//and two decimal places).  The purpose of this method is to get the user's selection from the
-	//submenu and return it.
+	/******************************************************************************** 
+	* Method Name: displaySubMenu <br> 
+	* Method Purpose:  Display a sub menu showing the orders that are currently in the 'system' and shows details of that order.   
+	*      <br> 
+	* 
+	* <hr> 
+	* Date created: 12/07/17 <br> 
+	* Date last modified: 12/07/17 <br> 
+	* 
+	* <hr> 
+	* 
+	*  
+	*  
+	*@param order1 CustomerOrder
+	*@param order2 CustomerOrder
+	*@param order3 CustomerOrder
+	*@param numOrders int
+	*@return selection
+	* <hr> 
+	*/
+	
 	public static int displaySubMenu (CustomerOrder order1, CustomerOrder order2, CustomerOrder order3,
 									  int numOrders)
 	{
-		int selection=0;
+		int selection = 0;
+		String input;
 		
+		String menu = "";
+		
+		if(numOrders == 1) {
+			
+			   menu = ("    Orders   \n"
+	  			     + "---------------\n"
+					 + "1: " + order1.getCustomerName() + " Balance Due: " + order1.format(order1.TotalDue())
+					 );
+			   
+				input = JOptionPane.showInputDialog(menu);
+				selection = Integer.parseInt(input);
+				
+				while(selection >= 2 && selection <= 0) {
+					input = JOptionPane.showInputDialog(menu);
+					selection = Integer.parseInt(input);
+				}}
+				
+			else if(numOrders == 2) {
+					
+				   menu = ("    Orders   \n"
+			  		     + "---------------\n"
+						 + "1: " + order1.getCustomerName() + " Balance Due: " + order1.format(order1.TotalDue())
+						 + "\n2: " + order2.getCustomerName() + " Balance Due: " + order2.format(order2.TotalDue())
+						 );
+					   
+					input = JOptionPane.showInputDialog(menu);
+					selection = Integer.parseInt(input);
+						
+					while(selection >= 3 && selection <= 0) {
+						input = JOptionPane.showInputDialog(menu);
+						selection = Integer.parseInt(input);
+					}}
+			else {
+				
+				   menu = ("    Orders   \n"
+			  		     + "---------------\n"
+						 + "1: " + order1.getCustomerName() + " Balance Due: " + order1.format(order1.TotalDue())
+						 + "\n2: " + order2.getCustomerName() + " Balance Due: " + order2.format(order2.TotalDue())
+						 + "\n3: " + order3.getCustomerName() + " Balance Due: " + order3.format(order3.TotalDue())
+						 );
+					   
+					input = JOptionPane.showInputDialog(menu);
+					selection = Integer.parseInt(input);
+						
+					while(selection >= 4 && selection <= 0) {
+						input = JOptionPane.showInputDialog(menu);
+						selection = Integer.parseInt(input);
+					}}					
 		return selection;
 	}
 	
-	//This method is written for you and complete.  You simply need to call it whenever a new order 
-	//is being input. This method asks the user information about an order and creates an order
-	//object that is then returned to the calling method to be stored in a variable there. All of
-	//the code to get the info and create the object is in this method and does not need to be 
-	//duplicated elsewhere.
+	/******************************************************************************** 
+	* Method Name: inputOrder <br> 
+	* Method Purpose:  Shows multiple windows asking for attributes of a Customer Order object.   
+	*      <br> 
+	* 
+	* <hr> 
+	* Date created: 12/07/17 <br> 
+	* Date last modified: 12/07/17 <br> 
+	* 
+	* <hr> 
+	* 
+	*  
+	*  
+	*
+	*@return temp
+	* <hr> 
+	*/
 	public static CustomerOrder inputOrder()
 	{
-		
-		//I inadvertently left off the prompt in the skeleton program for the customer number.  
-		//So, you will need to go to the inputOrder method and add the prompt for that.  
-		//Just follow the example of how everything else is being prompted for and passed to the setter.
 		CustomerOrder temp = new CustomerOrder();
+		
+		temp.setCustomerNumber(JOptionPane.showInputDialog("Enter customer number: "));
 		
 		temp.setCustomerName
 				(JOptionPane.showInputDialog("Enter your name:"));
